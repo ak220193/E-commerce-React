bd237dd9e0a52eea548209bcfed42f1da2767757
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-15 20:23:14 +0530

    Banner section Completed

--- client/src/Components/Slider/Slider.jsx
+++ client/src/Components/Slider/Slider.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import Image1 from "../../Assets/image1.png";
+import Image2 from "../../Assets/image2.png";
+import Image3 from "../../Assets/women1.jpg";
+import Image4 from "../../Assets/women5.jpg";
+
 import "./Slider.css";
 
-
 const Slider = () => {
-  return <div className="Slider__Container"></div>;
+
+  const banneritems = [
+    {
+      id: 1,
+      img: Image1,
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: Image2,
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: Image3,
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+    {
+      id: 4,
+      img: Image4,
+      title: "ETHINIC MELA",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 20% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+    const totalSlides = banneritems.length;
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : totalSlides - 1);
+    } else {
+      setSlideIndex(slideIndex < totalSlides - 1 ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <div className="Slider__container">
+      <div className="arrow left" onClick={() => handleClick("left")}>
+        <IoIosArrowBack />
+      </div>
+      <div className="wrapper" style={{ transform: `translateX(${slideIndex * -100}vw)` }}>
+        {banneritems.map((item) => (
+          <div className="slide" style={{ backgroundColor: `#${item.bg}` }} key={item.id}>
+            <div className="imgContainer">
+              <img src={item.img} alt="slide" className="image" />
+            </div>
+            <div className="infoContainer">
+              <h1 className="title">{item.title}</h1>
+              <p className="desc">{item.desc}</p>
+              <button className="button">SHOW NOW</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="arrow right" onClick={() => handleClick("right")}>
+        <IoIosArrowForward />
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
--- client/src/Variabledata/bannerdata.jsx
+++ client/src/Variabledata/bannerdata.jsx
@@ -1,24 +1,33 @@
+
+
 export const banneritems = [
 
     {
         id: 1,
-        img: "https://i.ibb.co/XsdmR2c/1.png",
+        img: "../Assets/women1.jpg",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "f5fafd",
       },
       {
         id: 2,
-        img: "https://i.ibb.co/DG69bQ4/2.png",
+        img: "../Assets/image1.png",
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "fcf1ed",
       },
       {
         id: 3,
-        img: "https://i.ibb.co/cXFnLLV/3.png",
+        img: "../Assets/image2.png",
         title: "LOUNGEWEAR LOVE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+      },
+      {
+        id: 4,
+        img: "../Assets/women5.jpg",
+        title: "ETHINIC MELA",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 20% OFF FOR NEW ARRIVALS.",
         bg: "fbf0f4",
       },
   
