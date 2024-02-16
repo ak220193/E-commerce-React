ae2f9a64ca211d03e824931d8f81adcb8d48823d
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-16 08:47:22 +0530

    too many issues in the code

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar/Navbar';
 import Smallnavbar from '../Components/Navbar/Smallnavbar';
 import Slider from '../Components/Slider/Slider';
+import Categories from '../Components/Categories/Categories';
 
 const Homepage = () => {
   return (
@@ -9,7 +10,7 @@ const Homepage = () => {
     <Smallnavbar/>
     <Navbar/>
     <Slider/>
-    
+    <Categories/>
     </div>
   )
 }
