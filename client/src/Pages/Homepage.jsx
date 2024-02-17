d8582053af044a3d77642def55c0faa6ee19ebf0
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-17 07:02:53 +0530

    slider changes

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../Components/Navbar/Navbar';
 import Smallnavbar from '../Components/Navbar/Smallnavbar';
 import Slider from '../Components/Slider/Slider';
-import Categories from '../Components/Categories/Categories';
 
 const Homepage = () => {
   return (
@@ -10,7 +9,6 @@ const Homepage = () => {
     <Smallnavbar/>
     <Navbar/>
     <Slider/>
-    <Categories/>
     </div>
   )
 }
