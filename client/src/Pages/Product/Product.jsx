9741ad7f6601dc1388f78565cb21f220739f93b6
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-19 23:15:44 +0530

    folder structure added for Pages

--- client/src/Pages/Product/Product.jsx
+++ client/src/Pages/Product/Product.jsx
@@ -3,11 +3,14 @@ import "./Product.css"
 
 import Navbar from '../../Components/Navbar/Navbar';
 import Smallnavbar from '../../Components/Navbar/Smallnavbar';
+import Footer from "../../Components/Footer/Footer"
 const Product = () => {
   return (
     <div className='Product_container'> 
     <Smallnavbar/>
     <Navbar/>
+    
+    <Footer/>
     </div>
   )
 }
