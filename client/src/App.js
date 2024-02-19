9f26fc7c5449db2de0c4d9ac82ef3a559b2689a3
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-19 22:54:26 +0530

    Product filter page completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Homepage from './Pages/Homepage';
+import ProductList from './Pages/Productlist/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      
+      <ProductList/>
     </div>
   );
 }
---
+++ client/src/Pages/Productlist/ProductList.jsx
@@ -0,0 +1,58 @@
+// ProductList.jsx
+
+import React from "react";
+import "./Productlist.css";
+import Navbar from "../../Components/Navbar/Navbar";
+import SmallNavbar from "../../Components/Navbar/Smallnavbar";
+import Products from "../../Components/Products/Products";
+import Newsletter from "../../Components/Newsletter/Newletter";
+import Footer from "../../Components/Footer/Footer";
+
+const ProductList = () => {
+  return (
+    <div className="container">
+      <Navbar />
+      <SmallNavbar />
+      <h1 className="title">Dresses</h1>
+      <div className="filter-container">
+        <div className="filter">
+          <span className="filter-text">Filter Products:</span>
+          <select className="select">
+            <option disabled selected>
+              Color
+            </option>
+            <option>White</option>
+            <option>Black</option>
+            <option>Red</option>
+            <option>Blue</option>
+            <option>Yellow</option>
+            <option>Green</option>
+          </select>
+          <select className="select">
+            <option disabled selected>
+              Size
+            </option>
+            <option>XS</option>
+            <option>S</option>
+            <option>M</option>
+            <option>L</option>
+            <option>XL</option>
+          </select>
+        </div>
+        <div className="filter">
+          <span className="filter-text">Sort Products:</span>
+          <select className="select">
+            <option selected>Newest</option>
+            <option>Price (asc)</option>
+            <option>Price (desc)</option>
+          </select>
+        </div>
+      </div>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default ProductList;
