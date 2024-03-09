ee719053d6d68c270ec0a36bf92e080b76c0ee1b
ak220193 <mail2meak22frcrio@gmail.com>
2024-03-09 22:49:34 +0530

    login page created without functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Homepage from './Pages/Homepage';
 import ProductList from './Pages/Productlist/ProductList';
 import Product from "./Pages/Product/Product.jsx"
+import Login from "./Pages/Login/Login.jsx";
+import Register from "./Pages/Register/Register.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Product/>
-      
+     
+      <Login />
     </div>
   );
 }
--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -3,7 +3,20 @@ import "./Login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className="login-container">
+    <div className="login-wrapper">
+      <h1 className="login-title">SIGN IN</h1>
+      <form className="login-form">
+        <input className="input" type="text" placeholder="username" />
+        <input className="input" type="password" placeholder="password" />
+        <button className="login-button">LOGIN</button>
+        <a className="link" href="#">DO NOT YOU REMEMBER THE PASSWORD?</a>
+        <a className="link" href="#">CREATE A NEW ACCOUNT</a>
+      </form>
+    </div>
+  </div>
+</>
   )
 }
 
