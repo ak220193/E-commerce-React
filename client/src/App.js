9862eba9607814979df6a16078ba0417557a6817
ak220193 <mail2meak22frcrio@gmail.com>
2024-03-09 23:28:30 +0530

    cart module

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import ProductList from './Pages/Productlist/ProductList';
 import Product from "./Pages/Product/Product.jsx"
 import Login from "./Pages/Login/Login.jsx";
 import Register from "./Pages/Register/Register.jsx";
+import Cart from "./Pages/Cart/Cart.jsx"
 
 function App() {
   return (
     <div className="App">
-     
-      <Register/>
+      <Cart/>
     </div>
   );
 }
