7bf8167cb1ac1a74627c34e10a253583622540b1
ak220193 <mail2meak22frcrio@gmail.com>
2024-03-09 23:27:19 +0530

    register page completed

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import "./Register.css"
+import React from "react";
+import "./Register.css";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="Register-container">
+      <div className="Register-wrapper">
+        <h1 className="Register-title">CREATE AN ACCOUNT</h1>
+        <form className="Register-form">
+          <input
+            className="input"
+            id="Register-input"
+            type="text"
+            placeholder="name"
+          />
+          <input
+            className="input"
+            id="Register-input"
+            type="text"
+            placeholder="last name"
+          />
+          <input
+            className="input"
+            id="Register-input"
+            type="text"
+            placeholder="username"
+          />
+          <input
+            className="input"
+            id="Register-input"
+            type="email"
+            placeholder="email"
+          />
+          <input
+            className="input"
+            id="Register-input"
+            type="password"
+            placeholder="password"
+          />
+          <input
+            className="Register-input"
+            type="password"
+            placeholder="confirm password"
+          />
+          <span className="agreement">
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the Privacy policy.
+          </span>
+          <button className="Register-button">CREATE</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
